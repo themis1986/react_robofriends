6853de730d7026155bf16d1a12d56b4daed4e2c9
thiakovidis <thiakovidis@agroapps.gr>
2022-08-03 14:10:13 +0300

    Replaced local data with fetched data from fake api with fetch()

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 
-import { robots } from "./robots";
-import "./App.css"
+// import { robots } from "./robots";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: "",
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ robots: users }));
   }
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value });
@@ -22,13 +27,18 @@ class App extends React.Component {
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
